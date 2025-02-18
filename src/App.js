ff1749b963ebb90d7e9fd67a5c99da53ee3daf6a
Mrinmoy Adhikary <63409502+mrinmoy96@users.noreply.github.com>
2025-02-18 17:56:41 +0530

    update app.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
-import DisplayName from './DisplayName';
+// import DisplayName from './DisplayName';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <DisplayName/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [first, setFirst] = useState("");
+  const [last, setLast] = useState("");
+  const [fullName, setFullName] = useState("");
+
+  const handleFirst = (e) => {
+    setFirst(e.target.value);
+  };
+
+  const handleLast = (e) => {
+    setLast(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!first || !last) {
+      setFullName("");
+      return;
+    }
+    let res = first + " " + last;
+    setFullName(res);
+  };
   return (
-    <div className="App">
-      <DisplayName/>
-    </div>
+    <>
+      <div className="App">
+        <h1>Full Name Display</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <p>First Name:</p>
+            <input type="text" value={first} onChange={(e) => handleFirst(e)} />
+          </div>
+          <div>
+            <p>Last Name:</p>
+            <input type="text" value={last} onChange={(e) => handleLast(e)} />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+        {fullName && <div>Full Name: {fullName}</div>}
+      </div>
+    </>
   );
 }
 
